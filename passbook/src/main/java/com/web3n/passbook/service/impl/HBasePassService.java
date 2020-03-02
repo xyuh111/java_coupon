93a6b8d37f34281449d2632f23d0ba7bb474f113
huidy <i@xieyuhui.com>
2020-03-02 16:11:29 +0800

    6-14 商户投放优惠劵入 HBase 功能实现

---
+++ passbook/src/main/java/com/web3n/passbook/service/impl/HBasePassService.java
@@ -0,0 +1,64 @@
+package com.web3n.passbook.service.impl;
+
+import com.spring4all.spring.boot.starter.hbase.api.HbaseTemplate;
+import com.web3n.passbook.constant.Constants;
+import com.web3n.passbook.service.IHBasePassService;
+import com.web3n.passbook.utils.RowKeyGenUtil;
+import com.web3n.passbook.vo.PassTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.lang.time.DateUtils;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * Created by macro on HBasePassService.
+ * Pass HBase 服务
+ **/
+@Slf4j
+@Service
+public class HBasePassService implements IHBasePassService {
+    /**
+     * HBase 客户端
+     */
+    @Autowired
+    private HbaseTemplate hbaseTemplate;
+
+    @Override
+    public boolean dropPassTemplateToHBase(PassTemplate passTemplate) {
+        if(null == passTemplate){
+            return false;
+        }
+        String rowKey = RowKeyGenUtil.genPassTemplateRowKey(passTemplate);
+        try{
+            if(hbaseTemplate.getConnection().getTable(TableName.valueOf(Constants.PassTemplateTable.TABLE_NAME)).exists(new Get(Bytes.toBytes(rowKey)))){
+                log.warn("RowKey {} is already exist! ", rowKey);
+                return false;
+            }
+        } catch (Exception e) {
+            log.error("dropPassTemplateToHBase Error: {}", e.getMessage());
+            return false;
+        }
+        /** 写入 HBase */
+        Put put = new Put(Bytes.toBytes(rowKey));
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_B), Bytes.toBytes(Constants.PassTemplateTable.ID), Bytes.toBytes(passTemplate.getId()));
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_B), Bytes.toBytes(Constants.PassTemplateTable.TITLE), Bytes.toBytes(passTemplate.getTitle()));
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_B), Bytes.toBytes(Constants.PassTemplateTable.SUMMARY), Bytes.toBytes(passTemplate.getSummary()));
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_B), Bytes.toBytes(Constants.PassTemplateTable.DESC), Bytes.toBytes(passTemplate.getDesc()));
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_B), Bytes.toBytes(Constants.PassTemplateTable.HAS_TOKEN), Bytes.toBytes(passTemplate.getHasToken()));
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_B), Bytes.toBytes(Constants.PassTemplateTable.BACKGROUND), Bytes.toBytes(passTemplate.getBackground()));
+
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_C), Bytes.toBytes(Constants.PassTemplateTable.LIMIT), Bytes.toBytes(passTemplate.getLimit()));
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_C), Bytes.toBytes(Constants.PassTemplateTable.START), Bytes.toBytes(DateFormatUtils.ISO_DATE_FORMAT.format(passTemplate.getStart())));
+        put.addColumn(Bytes.toBytes(Constants.PassTemplateTable.FAMILY_C), Bytes.toBytes(Constants.PassTemplateTable.END), Bytes.toBytes(DateFormatUtils.ISO_DATE_FORMAT.format(passTemplate.getEnd())));
+        /** 保存或更新数据 */
+        hbaseTemplate.saveOrUpdate(Constants.PassTemplateTable.TABLE_NAME, put);
+        return true;
+    }
+}
