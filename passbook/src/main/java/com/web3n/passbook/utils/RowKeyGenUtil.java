5ff5724794450a789cc3af86f3c989f4f8ffd657
huidy <i@xieyuhui.com>
2020-03-02 14:45:59 +0800

    HBase 表 RowKey 生成实现

---
+++ passbook/src/main/java/com/web3n/passbook/utils/RowKeyGenUtil.java
@@ -0,0 +1,35 @@
+package com.web3n.passbook.utils;
+
+import com.web3n.passbook.vo.Feedback;
+import com.web3n.passbook.vo.PassTemplate;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import static org.apache.kafka.common.requests.FetchMetadata.log;
+
+/**
+ * Created by macro on RowKeyGenUtil.
+ * RowKey 生成器工具类
+ **/
+public class RowKeyGenUtil {
+    /**
+     * 根据提供的 PassTemplate 对象生成 RowKey
+     * @param passTemplate {@link PassTemplate}
+     * @return String RowKey
+     */
+    public static String genPassTemplateRowKey(PassTemplate passTemplate){
+        String passInfo = String.valueOf(passTemplate.getId()) + "_" + passTemplate.getTitle();
+        String rowKey = DigestUtils.md5Hex(passInfo);
+        log.info("GenPassTemplateRowKey:{},{}", passInfo, rowKey);
+        return rowKey;
+    }
+
+    /**
+     * 根据 Feedback 构造 RowKey
+     * @param feedback {@link Feedback}
+     * @return String RowKey
+     */
+    public static String genFeedbackRowKey(Feedback feedback){
+        return new StringBuilder(String.valueOf(feedback.getUserId())).reverse().toString() + (Long.MAX_VALUE - System.currentTimeMillis());
+    };
+
+}
