42828930cf8994729a0c3da6f35ab6943acb6531
huidy <i@xieyuhui.com>
2020-03-05 15:32:30 +0800

    6-25 优惠券库存服务功能实现

--- passbook/src/main/java/com/web3n/passbook/service/impl/InventoryServiceImpl.java
+++ passbook/src/main/java/com/web3n/passbook/service/impl/InventoryServiceImpl.java
@@ -2,11 +2,13 @@ package com.web3n.passbook.service.impl;
 
 import com.spring4all.spring.boot.starter.hbase.api.HbaseTemplate;
 import com.web3n.passbook.constant.Constants;
+import com.web3n.passbook.dao.MerchantsDao;
+import com.web3n.passbook.entity.Merchants;
 import com.web3n.passbook.mapper.PassTemplateRowMapper;
 import com.web3n.passbook.service.IInventoryService;
+import com.web3n.passbook.service.IUserPassService;
 import com.web3n.passbook.utils.RowKeyGenUtil;
-import com.web3n.passbook.vo.PassTemplate;
-import com.web3n.passbook.vo.Response;
+import com.web3n.passbook.vo.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.filter.CompareFilter;
@@ -17,9 +19,8 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @create 2020-03-05 14:49
@@ -32,10 +33,24 @@ public class InventoryServiceImpl implements IInventoryService {
     /** HBase 客户端 */
     @Autowired
     private HbaseTemplate hbaseTemplate;
+    /** MerchantsDao 接口 */
+    @Autowired
+    private MerchantsDao merchantsDao;
+    
+    @Autowired
+    private IUserPassService userPassService;
     
     @Override
+    @SuppressWarnings("unchecked") /** 把强转警告注释 (List<PassInfo>) */
     public Response getInventoryInfo(Long userId) throws Exception {
-        return null;
+        Response allUserPass = userPassService.getUserAllPassInfo(userId);
+        List<PassInfo> passInfos = (List<PassInfo>) allUserPass.getData();
+        List<PassTemplate> excludeObject = passInfos.stream().map(PassInfo::getPassTemplate).collect(Collectors.toList());
+        List<String> excludeIds = new ArrayList<>();
+        excludeObject.forEach(e -> excludeIds.add(
+                RowKeyGenUtil.genPassTemplateRowKey(e)
+        ));
+        return new Response(new InventoryResponse(userId, buildPassTemplateInfo(getAvailablePassTemplate(excludeIds))));
     }
     
     /**
@@ -79,4 +94,29 @@ public class InventoryServiceImpl implements IInventoryService {
         }
         return availablePassTemplates;
     };
+    
+    /**
+     * 构造优惠券的信息
+     * @param passTemplates {@link PassTemplate}
+     * @return {@link PassTemplateInfo}
+     */
+    private List<PassTemplateInfo> buildPassTemplateInfo(List<PassTemplate> passTemplates){
+        Map<Integer, Merchants> merchantsMap = new HashMap<>();
+        List<Integer> merchantsIds = passTemplates.stream().map(
+                PassTemplate::getId
+        ).collect(Collectors.toList());
+        List<Merchants> merchants = merchantsDao.findByIdIn(merchantsIds);
+        merchants.forEach(m -> merchantsMap.put(m.getId(), m));
+        List<PassTemplateInfo> result = new ArrayList<>(passTemplates.size());
+        for (PassTemplate passTemplate : passTemplates) {
+            Merchants mc = merchantsMap.getOrDefault(passTemplate.getId(), null);
+            if (null == mc){
+                log.error("Merchants Error : {}", passTemplate.getId());
+                continue;
+            }
+            result.add(new PassTemplateInfo(passTemplate, mc));
+        }
+        
+        return result;
+    }
 }
